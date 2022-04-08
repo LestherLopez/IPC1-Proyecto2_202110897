ccf7652fd25179f3fb676bf481366a45c00692b8
LestherLopez <3010308470101@ingenieria.usac.edu.gt>
2022-04-08 00:31:13 -0600

    Json usuarios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+
 const app = express();
 app.set('port',4000);
+
+app.use(morgan('dev')); 
+app.use(express.json());
+app.use(cors())
+
 
 app.get('/home',(req,res)=>{
     res.send('Holaaa!!!!!');
@@ -10,6 +19,11 @@ app.get('/pokedex',(req,res)=>{
  //   let pokedex = require('./pokedex.json');
     res.send('pokedex!!!!');
 });
+
+app.get('/usuarios',(req,res)=>{
+    var db = require('./usuarios.json');
+    res.send(db);
+})
 
 app.listen(app.get('port'),()=>{
     console.log('Servidor iniciado en el puerto: 4000');
