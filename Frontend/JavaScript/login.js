24fcef2c3583fff0244a32ec6d23fc213e7992c9
LestherLopez <3010308470101@ingenieria.usac.edu.gt>
2022-04-16 20:36:35 -0600

    Login

--- Frontend/JavaScript/login.js
+++ Frontend/JavaScript/login.js
@@ -1,12 +1,22 @@
 function ingresar(){
+    let url = 'http://localhost:4000/usuarios'
     var username = document.getElementById('Usuario').value;
     var password=document.getElementById('Password').value;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => mostrarData(data))
+        .catch(error => console.log(error))
 
-    if(username=="admin"  && password=="admin"){
-       
-        document.location.assign('HTML/Home.html')
-    }
-    else{
-        alert("Error");
+    const mostrarData = (data) =>{
+        let acept = true;
+        for(let i=0; i<data.length; i++){ 
+            if(username==data[i].Usuario   && password==data[i].Password){
+                acept = false;
+                document.location.assign('HTML/Home.html')
+            }
+        }
+        if(acept){
+            alert("Error");
+        }
     }
 }
